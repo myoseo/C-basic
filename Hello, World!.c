7d1054d3b52ace7103cc973e4ece2efbe0663d8c
myoseo <162320712+myoseo@users.noreply.github.com>
2025-11-13 22:25:41 +0900

    Enhance comments in Hello, World! program
    Add multi-line comment explaining the code

--- Hello, World!.c
+++ Hello, World!.c
@@ -1,5 +1,9 @@
+//STandard Input Output header의 약자인 stdio.h라는 파일을 포함시키는(불러오는) 코드
 #include<stdio.h>
-
+/*
+아래는 코드는 단순한 "Hello, World!라는 문장을 출력하는 코드임
+위의 주석과 다르게 이 주석은 여러줄을 쓸 수 있음
+*/
 int main(){
     printf("Hello, World!\n");
     return 0;
